3ceffe55a1966a6d5b086a22d501c1f83fcb280a
Evan Brearley <eabrearl@uwaterloo.ca>
2017-09-25 21:33:17 -0400

    Popup.js now will not act if links are already fixed.

--- popup.js
+++ popup.js
@@ -7,15 +7,10 @@ function click(e) {
   
 	chrome.tabs.executeScript(null,
  {
-		code:"var postingsTable = document.getElementById('postingsTable'); var postings = postingsTable.getElementsByTagName('a'); for (var i = 0; i < postings.length; i++){if(postings[i].hasAttribute('onclick') && postings[i].getAttribute('onclick').includes('orbisApp.buildForm')){var end = postings[i].getAttribute('onclick').lastIndexOf(\x22'\x22); postings[i].setAttribute('onclick',postings[i].getAttribute('onclick').slice(0, end) + '_blank' + postings[i].getAttribute('onclick').slice(end, postings[i].getAttribute('onclick').length))}}"
+		code:"var postingsTable = document.getElementById('postingsTable'); var postings = postingsTable.getElementsByTagName('a'); for (var i = 0; i < postings.length; i++){if(postings[i].hasAttribute('onclick') && postings[i].getAttribute('onclick').includes('orbisApp.buildForm') && !postings[i].getAttribute('onclick').includes('_blank')){var end = postings[i].getAttribute('onclick').lastIndexOf(\x22'\x22); postings[i].setAttribute('onclick',postings[i].getAttribute('onclick').slice(0, end) + '_blank' + postings[i].getAttribute('onclick').slice(end, postings[i].getAttribute('onclick').length))}}"
 	});
   
 	window.close();
-	// prevent a second click from adding a second '_blank' with a second click and thereby breaking the links being changed to new tab
-	var divs = document.querySelectorAll('div');
-  
-	divs[0].addEventListener('click', null);
-
 }
 
 
